37607b25f86931a721b44d8c1ec379a97b69c496
João Pedro G. C <115590969+jpgercc@users.noreply.github.com>
2024-04-27 01:47:23 -0300

    Semi-Final

--- scripts.js
+++ scripts.js
@@ -1,17 +1,26 @@
 function scrollDown() {
-    // Altura desejada a rolar em porcentagem da altura total da página
-    const scrollAmountInPercent = 40; // Agora estamos rolando 70% para baixo
-    
-    // Obtém a altura total da página
-    const pageHeight = document.documentElement.scrollHeight - window.innerHeight;
-    
-    // Calcula a nova posição da rolagem em pixels
-    const targetPositionInPx = (scrollAmountInPercent / 100) * pageHeight;
-    
-    // Rola a página para a nova posição com animação suave (smooth scrolling)
-    window.scrollTo({
-      top: targetPositionInPx,
-      behavior: "smooth"
-    });
-  }
+  // Altura desejada a rolar em porcentagem da altura total da página
+  const scrollAmountInPercent = 25; // Agora estamos rolando X% para baixo
   
+  // Obtém a altura total da página
+  const pageHeight = document.documentElement.scrollHeight - window.innerHeight;
+  
+  // Calcula a nova posição da rolagem em pixels
+  const targetPositionInPx = (scrollAmountInPercent / 100) * pageHeight;
+  
+  // Rola a página para a nova posição com animação suave (smooth scrolling)
+  window.scrollTo({
+    top: targetPositionInPx,
+    behavior: "smooth"
+  });
+}
+
+function myFunction() {
+  var x = document.getElementById("myPara");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+}
+
